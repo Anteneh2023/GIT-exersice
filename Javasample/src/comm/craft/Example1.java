ae526dec508c2ff5558ec1fc03f139af1b54ad1e
Anteneh2023 <126181478+Anteneh2023@users.noreply.github.com>
2023-02-28 19:49:31 -0500

    Commit from Eclipes

---
+++ Javasample/src/comm/craft/Example1.java
@@ -0,0 +1,13 @@
+package comm.craft;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Print my first text");
+		System.out.println("Print my first text");
+		System.out.println("Print my first text");
+	}
+
+}
